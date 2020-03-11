6f765afb3f8b3fb0570e12b84028f422db8fa554
César Pomar <cesarpomar18@gmail.com>
2020-03-11 14:21:14 +0100

    backend stop must be oneway

--- ignis/rpc/driver/backend/IBackendService.py
+++ ignis/rpc/driver/backend/IBackendService.py
@@ -32,29 +32,13 @@ class Client(Iface):
 
     def stop(self):
         self.send_stop()
-        self.recv_stop()
 
     def send_stop(self):
-        self._oprot.writeMessageBegin('stop', TMessageType.CALL, self._seqid)
+        self._oprot.writeMessageBegin('stop', TMessageType.ONEWAY, self._seqid)
         args = stop_args()
         args.write(self._oprot)
         self._oprot.writeMessageEnd()
         self._oprot.trans.flush()
-
-    def recv_stop(self):
-        iprot = self._iprot
-        (fname, mtype, rseqid) = iprot.readMessageBegin()
-        if mtype == TMessageType.EXCEPTION:
-            x = TApplicationException()
-            x.read(iprot)
-            iprot.readMessageEnd()
-            raise x
-        result = stop_result()
-        result.read(iprot)
-        iprot.readMessageEnd()
-        if result.ex is not None:
-            raise result.ex
-        return
 
 
 class Processor(Iface, TProcessor):
@@ -88,27 +72,12 @@ class Processor(Iface, TProcessor):
         args = stop_args()
         args.read(iprot)
         iprot.readMessageEnd()
-        result = stop_result()
         try:
             self._handler.stop()
-            msg_type = TMessageType.REPLY
         except TTransport.TTransportException:
             raise
-        except ignis.rpc.driver.exception.ttypes.IDriverException as ex:
-            msg_type = TMessageType.REPLY
-            result.ex = ex
-        except TApplicationException as ex:
-            logging.exception('TApplication exception in handler')
-            msg_type = TMessageType.EXCEPTION
-            result = ex
         except Exception:
-            logging.exception('Unexpected exception in handler')
-            msg_type = TMessageType.EXCEPTION
-            result = TApplicationException(TApplicationException.INTERNAL_ERROR, 'Internal error')
-        oprot.writeMessageBegin("stop", msg_type, seqid)
-        result.write(oprot)
-        oprot.writeMessageEnd()
-        oprot.trans.flush()
+            logging.exception('Exception in oneway handler')
 
 # HELPER FUNCTIONS AND STRUCTURES
 
@@ -153,69 +122,6 @@ class stop_args(object):
         return not (self == other)
 all_structs.append(stop_args)
 stop_args.thrift_spec = (
-)
-
-
-class stop_result(object):
-    """
-    Attributes:
-     - ex
-
-    """
-
-
-    def __init__(self, ex=None,):
-        self.ex = ex
-
-    def read(self, iprot):
-        if iprot._fast_decode is not None and isinstance(iprot.trans, TTransport.CReadableTransport) and self.thrift_spec is not None:
-            iprot._fast_decode(self, iprot, [self.__class__, self.thrift_spec])
-            return
-        iprot.readStructBegin()
-        while True:
-            (fname, ftype, fid) = iprot.readFieldBegin()
-            if ftype == TType.STOP:
-                break
-            if fid == 1:
-                if ftype == TType.STRUCT:
-                    self.ex = ignis.rpc.driver.exception.ttypes.IDriverException()
-                    self.ex.read(iprot)
-                else:
-                    iprot.skip(ftype)
-            else:
-                iprot.skip(ftype)
-            iprot.readFieldEnd()
-        iprot.readStructEnd()
-
-    def write(self, oprot):
-        if oprot._fast_encode is not None and self.thrift_spec is not None:
-            oprot.trans.write(oprot._fast_encode(self, [self.__class__, self.thrift_spec]))
-            return
-        oprot.writeStructBegin('stop_result')
-        if self.ex is not None:
-            oprot.writeFieldBegin('ex', TType.STRUCT, 1)
-            self.ex.write(oprot)
-            oprot.writeFieldEnd()
-        oprot.writeFieldStop()
-        oprot.writeStructEnd()
-
-    def validate(self):
-        return
-
-    def __repr__(self):
-        L = ['%s=%r' % (key, value)
-             for key, value in self.__dict__.items()]
-        return '%s(%s)' % (self.__class__.__name__, ', '.join(L))
-
-    def __eq__(self, other):
-        return isinstance(other, self.__class__) and self.__dict__ == other.__dict__
-
-    def __ne__(self, other):
-        return not (self == other)
-all_structs.append(stop_result)
-stop_result.thrift_spec = (
-    None,  # 0
-    (1, TType.STRUCT, 'ex', [ignis.rpc.driver.exception.ttypes.IDriverException, None], None, ),  # 1
 )
 fix_spec(all_structs)
 del all_structs
