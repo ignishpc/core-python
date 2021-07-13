3b811e1bd740fb21ce1eaae73379871499939b4f
César Pomar <cesarpomar18@gmail.com>
2021-07-13 04:05:44 +0200

    Dockerfiles for the new build system

---
+++ Dockerfiles/python-builder/ignis-python-install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+apt update
+apt -y --no-install-recommends install python3.8 python3.8-distutils python3-numpy
+rm -rf /var/lib/apt/lists/*
+
+python3 ${IGNIS_HOME}/bin/get-pip.py
+rm -f ${IGNIS_HOME}/bin/get-pip.py
+python3 -m pip install certifi
+
+cd ${IGNIS_HOME}/core/python-libs/
+cd mpi4py
+python3 setup.py install
+cd ..
+cd thrift
+python3 setup.py install
+rm -fR ${IGNIS_HOME}/core/python-libs/
+
+cd ${IGNIS_HOME}/core/python/
+python3 setup.py develop
